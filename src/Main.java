b05d35c68b8afdefb1b0e12d17220cb8c6cf9556
İmranov <semtexpert@gmail.com>
2024-01-04 15:23:46 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dizinin boyutunu giriniz: ");
+        int size = input.nextInt();
+
+        int[] list = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print((i + 1) + ". Elemanı giriniz: ");
+            list[i] = input.nextInt();
+        }
+
+        Arrays.sort(list);
+
+        System.out.println("Sıralanmış Dizi: " + Arrays.toString(list));
     }
 }
